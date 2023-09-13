74603a63c1c1038740d0653f3d47db0cb6ee2303
RAFAHERNÁNDEZ25 <fikyhr@gmail.com>
2023-09-13 11:07:22 -0500

    User Card

--- src/pages/UserDetailPage.tsx
+++ src/pages/UserDetailPage.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-
+import './styles/UserDetailPage.css';
+import Userimg  from "./img/tumblr_mddh4dv3yc1r02l78.jpg";
 // React Router DOM Imports
 import { useNavigate, useParams } from 'react-router-dom';
 import { useSessionStorage } from '../hooks/useSessionStorage';
@@ -38,33 +39,60 @@ export const UserDetailPage = () => {
   // Variable to navigate
 
   return (
-    <div>
-      <h1>User Detail Page: {id}</h1>
-      {user ? (
-        <div className='user-data'>
-          <h2>{user.name}</h2>
-          <h3>{user.number}</h3>
-          <h3>{user.username}</h3>
-          <h3>{user.cedula}</h3>
-          <h3>{user.email}</h3>
-          <h3>{user.telefono}</h3>
-          <h3>{user.more_info}</h3>
-          {user.roles && user.roles.length > 0 && (
+    <div className=''>
+      <div className=''>
+            {user ? (
+            <div className='UserCard'>
+                  <section className="usercard-user-section">
+                  <div className="usercard" data-state="#about">
+                      <div className="usercard-header">
+                        <div className="usercard-cover" ></div>
+                        <img className='usercard-Avatar' src={Userimg} alt="" width="90px" height="80px" />
+                        <h2 className="usercard-fullname">{user.name}</h2>
+                        <h2 className="usercard-jobtitle">{user.more_info}</h2>     
+                      </div>
+                      <div className="usercard-main">
+                        <div className="usercard-section is-active" >
+                          <div className="usercard-content-1">
+                            <div className="usercard-subtitle">Nombre de usuario</div>
+                              <p className="usercard-desc">{user.username}</p>
+                            <div className="usercard-subtitle">Cedula</div>
+                              <p className="usercard-desc">{user.cedula}</p>
+                              {user.roles && user.roles.length > 0 && (
+                              <div>
+                                <p className='usercard-subtitle'>Roles</p>
+                                <ul>
+                                  {user.roles.map((role: any) => (
+                                    <p className='usercard-desc' key={role._id}>{role.name}</p>
+                                  ))}
+                                </ul>
+                              </div>
+                               )}
+                          </div>     
+
+                          <div className="usercard-content-2"> 
+                              <div className="usercard-subtitle">Numero id</div>
+                              <p className="usercard-desc">{id}</p>
+                            <div className="usercard-subtitle">Telefono</div>
+                              <p className="usercard-desc">{user.telefono}</p>                 
+                            <div className="usercard-subtitle">Email</div>
+                                <p className="usercard-desc">{user.email}</p> 
+                          </div>                
+                        </div>
+                        
+                      </div>
+                      <div className="usercard-buttons">
+                           <button className='usercard-button' data-section="#experience">More Info</button>
+                      </div>
+                    </div>
+                  </section>
+                  </div>           
+          ) : (
             <div>
-              <h3>Roles:</h3>
-              <ul>
-                {user.roles.map((role: any) => (
-                  <li key={role._id}>{role.name}</li>
-                ))}
-              </ul>
+              <h2>Loading data...</h2>
             </div>
-          )}
-        </div>
-      ) : (
-        <div>
-          <h2>Loading data...</h2>
-        </div>
-      )}
+             )}
+      </div>
     </div>
   );
 };
